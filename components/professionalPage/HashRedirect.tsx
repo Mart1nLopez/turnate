3e6d94574cfb02095f53106793187b371308e812
Lucas <lucasbenji@hotmail.com>
2025-06-22 00:06:39 -0400

    fix: Solucionar error que cuando entraba con cualquier # mandaba al confirmado email

--- components/professionalPage/HashRedirect.tsx
+++ components/professionalPage/HashRedirect.tsx
@@ -5,7 +5,13 @@ import { useEffect } from 'react';
 export default function HashRedirect() {
   useEffect(() => {
     if (typeof window !== 'undefined' && window.location.hash) {
-      window.location.replace('/auth/confirmado');
+      const hash = window.location.hash.substring(1); // Quita el '#'
+      const params = new URLSearchParams(hash);
+      const access_token = params.get('access_token');
+      if (access_token) {
+        window.localStorage.setItem('access_token', access_token);
+        window.location.replace('/auth/confirmado');
+      }
     }
   }, []);
 
